512e0f84b47bde4fd8c32e0f6e63c6c9a389bb90
Kejender <perttu.makkonen@gmail.com>
2025-03-12 17:28:16 +0200

    test case added

--- components/ToDoList.test.jsx
+++ components/ToDoList.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import ToDoList from './ToDoList'
 
 test('renders content', () => {
@@ -12,4 +13,20 @@ test('renders content', () => {
 
   const element = screen.getByText('Component testing is done with react-testing-library')
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const todo = {
+    name: 'Component testing is done with react-testing-library'
+  }
+  
+  const mockHandler = vi.fn()
+
+  render(<ToDoList key={todo.id} todo={todo} showTaskView={mockHandler}/>)
+
+  const user = userEvent.setup()
+  const button = screen.getByText('Component testing is done with react-testing-library')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
